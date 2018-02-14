7dfc1553e197ebb15c883d7e549e96fc0a6d6f33
Aniket-Singla <36408298+Aniket-Singla@users.noreply.github.com>
2018-02-14 16:31:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var infoone ={
+    title:"Description",
+    heading:"Description of My webapp",
+    date:14-02-2017,
+    content:`<p>
+                Hello friends welcome to my webapp. This page is made to describe my webapp. This is the first webapp I made using Hasaura's Console.
+            </p>
+            <p>
+                These are the very good lectures given by Tanmai sir. These lectures provide very good introduction to the web-applications.
+            </p>`
+};
+function createTemplate(data){
+    var title= data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+    
+    var htmlTemplate=`
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name= "viewport" content="width=device-width, initial-scale=1">
+            <link href="ui/style.css" rel="stylesheet">
+        </head>
+        <body>
+            <div class="container">
+                <div>
+            <a href="/">Go to Home</a>
+            </div>
+            <hr/>
+            <div>
+                <h1>
+                    ${heading}
+                </h1>
+                <br/>Date Edited: 14-02-2018<br/>
+                
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/description',(req,res)=>{
-    res.sendFile(path.join(__dirname,'ui','desc.html'));
+    res.send(createTemplate(infoone));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
