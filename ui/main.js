2fa8e7f32abad34c992856b330e433d753927e57
Aniket-Singla <36408298+Aniket-Singla@users.noreply.github.com>
2018-02-15 18:25:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 var img1 = document.getElementById('img');
 var marginval= 0;
 function margin(){
-    return marginval = marginval +10;
+    return marginval = marginval +1;
 }
 function moveright(){
     img1.style.marginLeft=margin()+'px';
 }
 img1.onclick= function(){
-    var interval = setInterval(moveright,50);
+    var interval = setInterval(moveright,100);
     
 };
