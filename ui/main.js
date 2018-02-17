cbb368c539253355f171d6928dec4e24d30e2114
Aniket-Singla <36408298+Aniket-Singla@users.noreply.github.com>
2018-02-17 12:43:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var img1 = document.getElementById('img');
 var marginval= 0;
 function margin(){
-    if (marginval===100){
+    if (marginval===500){
         marginval= 0;
     }
     return marginval = marginval +1;
