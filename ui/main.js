ca9aa431d1a82346a4c2c82ec74a0707cd2a020d
Aniket-Singla <36408298+Aniket-Singla@users.noreply.github.com>
2018-02-17 13:03:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,10 @@ img1.onclick= function(){
     var interval = setInterval(moveright,10);
     
 };
+var val= 0;
+var button = document.getElementById('counter');
+var count = document.getElementById('count');
+button.onClick= function(){
+    val = val+1;
+    count.innerHtml= val;
+};
